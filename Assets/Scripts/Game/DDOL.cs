f5cfd364fce1d8e5c878290e670780fd0b490c68
Audric <audric.steibel@gmail.com>
2020-05-25 18:08:58 +0200

    added preload and ddol

---
+++ Assets/Scripts/Game/DDOL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DDOL : MonoBehaviour
+{
+
+    public GameObject[] gameObjects;
+
+    private void Awake()
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            DontDestroyOnLoad(go);
+        }
+        SceneManager.LoadScene("GameScene");
+    }
+}
