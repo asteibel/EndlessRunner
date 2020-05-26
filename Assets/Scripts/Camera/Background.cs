ec6bf2c88d45f6d32f60b6fd469f24e7071db93a
Audric <audric.steibel@gmail.com>
2020-05-26 18:34:57 +0200

    added background

---
+++ Assets/Scripts/Camera/Background.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    private float length, startPosition;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distanceToCamera = cam.transform.position.x * (1f - parallaxEffect);
+        float distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (distanceToCamera > startPosition + length) startPosition += length;
+        else if (distanceToCamera < startPosition - length) startPosition -= length;
+    }
+}
