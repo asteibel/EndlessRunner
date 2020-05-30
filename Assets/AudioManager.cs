953f50f81159f39d858896c8c5045f0a9ddbb771
Audric <audric.steibel@gmail.com>
2020-05-30 17:08:11 +0200

    added analytics

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -21,7 +21,6 @@ public class AudioManager : MonoBehaviour
 
     public void Play(string name)
     {
-        Debug.Log("playing " + name);
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null)
         {
@@ -33,7 +32,6 @@ public class AudioManager : MonoBehaviour
 
     public void Stop(string name)
     {
-        Debug.Log("stop playing " + name);
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null)
         {
--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
 
-    private GameManager gameManager;
     private SpriteRenderer graphics;
 
     private void Awake()
     {
-        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         graphics = GetComponent<SpriteRenderer>();
         graphics.flipX = Random.value < 0.5f;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GameManager gameManager = GameManager.instance;
         if (collision.CompareTag("Player"))
         {
             gameManager.GameIsOver();
+
+            string levelPartName;
+            if  (transform.parent != null)
+            {
+                levelPartName = transform.parent.name;
+            } else
+            {
+                levelPartName = "no_parent";
+                
+            }
+            Debug.Log("has hit an obsctable in " + levelPartName);
+
+            int distance = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentDistance;
+            int highScore = gameManager.gameData.highScore;
+
+            Firebase.Analytics.Parameter[] gameOVerParameters = {
+                new Firebase.Analytics.Parameter(
+                "LevelPart", levelPartName),
+                new Firebase.Analytics.Parameter(
+                 "DeathType", "obstacle"),
+                new Firebase.Analytics.Parameter(
+                 "Score", distance),
+                new Firebase.Analytics.Parameter(
+                 "HighScore", highScore)
+                };
+            Firebase.Analytics.FirebaseAnalytics.LogEvent("GameOver", gameOVerParameters);
         }
     }
 }
