6b384df79dd1a3d0c7e2d334e0900abff8183c91
Audric <audric.steibel@gmail.com>
2020-06-01 18:03:48 +0200

    update character

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -7,10 +7,29 @@ public class Obstacle : MonoBehaviour
 
     private SpriteRenderer graphics;
 
+    // chance to keep cactus
+    // 0 -> 250, 50%; 250 -> 500: 60%; 500 -> 1000: 75%; 1000 -> 1500: 90%; 1500+ 100%.
+
     private void Awake()
     {
         graphics = GetComponent<SpriteRenderer>();
         graphics.flipX = Random.value < 0.5f;
+        Debug.Log("spawning catctus");
+        float shouldDestroyCactus = Random.value;
+        int currentDistance = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentDistance;
+        if (currentDistance < 250 && shouldDestroyCactus < 0.5f)
+        {
+            Destroy(gameObject);
+        } else if (currentDistance < 500 && shouldDestroyCactus < 0.4f)
+        {
+            Destroy(gameObject);
+        } else if (currentDistance < 1000 && shouldDestroyCactus < 0.35f)
+        {
+            Destroy(gameObject);
+        } else if (currentDistance < 1500 && shouldDestroyCactus < 0.1f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
