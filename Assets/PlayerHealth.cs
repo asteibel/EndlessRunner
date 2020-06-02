ff8f8e408f2b718eb3a131abed3fcb94895eb969
Audric <audric.steibel@gmail.com>
2020-06-02 17:39:27 +0200

    wip life system

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int numberOfLives = 3;
+
+    /**
+     * return if the game is over
+     * */
+    public bool TakeDamage(int damagetToTake)
+    {
+        numberOfLives -= damagetToTake;
+        if (numberOfLives <= 0)
+        {
+            GameManager.instance.GameIsOver();
+            return true;
+        }
+        return false;
+    }
+
+    public void ResetHealth(int _numberOfLives) 
+    {
+        numberOfLives = _numberOfLives;
+    }
+}
