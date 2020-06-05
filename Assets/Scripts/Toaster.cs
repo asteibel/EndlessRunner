95156c61a4ec8abfd9efd352a53d8eafc33f10aa
Audric <audric.steibel@gmail.com>
2020-06-05 16:37:49 +0200

    showing GPGS on success

---
+++ Assets/Scripts/Toaster.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Toaster : MonoBehaviour
+{
+
+    public static Toaster instance;
+    public TextMeshProUGUI txt;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("more than one toaster");
+            return;
+        }
+        instance = this;
+        txt.enabled = false;
+    }
+
+    public void ShowToast(string text, int duration)
+    {
+        StartCoroutine(showToastCOR(text, duration));
+    }
+
+    private IEnumerator showToastCOR(string text,
+        int duration)
+    {
+
+        txt.text = text;
+        txt.enabled = true;
+
+        //Fade in
+        yield return fadeInAndOut(txt, true, 0.5f);
+
+        //Wait for the duration
+        float counter = 0;
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
+        //Fade out
+        yield return fadeInAndOut(txt, false, 0.5f);
+
+        txt.enabled = false;
+    }
+
+    IEnumerator fadeInAndOut(TextMeshProUGUI targetText, bool fadeIn, float duration)
+    {
+        //Set Values depending on if fadeIn or fadeOut
+        float a, b;
+        if (fadeIn)
+        {
+            a = 0f;
+            b = 1f;
+        }
+        else
+        {
+            a = 1f;
+            b = 0f;
+        }
+
+        Color currentColor = Color.clear;
+        float counter = 0f;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            float alpha = Mathf.Lerp(a, b, counter / duration);
+
+            targetText.color = new Color(1f, 1f, 1f, alpha);
+            yield return null;
+        }
+    }
+}
