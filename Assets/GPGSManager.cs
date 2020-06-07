3a0a2382a828bb63ed14df0f79554b95a4d703d3
Audric <audric.steibel@gmail.com>
2020-06-07 16:43:35 +0200

    added achievements

--- Assets/GPGSManager.cs
+++ Assets/GPGSManager.cs
@@ -10,7 +10,9 @@ public class GPGSManager : MonoBehaviour
 
     public static void Authenticate(System.Action<bool> callback)
     {
-        
+
+        PlayGamesClientConfiguration.Builder test = new PlayGamesClientConfiguration.Builder();
+        test.IsHidingPopups();
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
         PlayGamesPlatform.InitializeInstance(config);
         PlayGamesPlatform.Activate();
@@ -59,6 +61,17 @@ public class GPGSManager : MonoBehaviour
 
     public static class Achievements
     {
-        public static string FIRST_RUN = "CgkIkoHks9QfEAIQCA";
+        public static string NOVICE = "CgkIkoHks9QfEAIQCA";
+        public static string ADEPT = "CgkIkoHks9QfEAIQCQ";
+        public static string PRO = "CgkIkoHks9QfEAIQCg";
+        public static string EXPERT = "CgkIkoHks9QfEAIQCw";
+
+        public static string NICE_RUN = "CgkIkoHks9QfEAIQDA";
+        public static string GREAT_RUN = "CgkIkoHks9QfEAIQDQ";
+        public static string AWESOME_RUN = "CgkIkoHks9QfEAIQDg";
+        public static string EPIC_RUN = "CgkIkoHks9QfEAIQDw";
+        public static string LEGENDARY_RUN = "CgkIkoHks9QfEAIQEA";
+
+        public static string CACTUS_LOVER = "CgkIkoHks9QfEAIQEQ";
     }
 }
